c7a1df4dfc1f81a40f8c7bcb558c8589c7b5569a
Electric1447 <itai.levin4@gmail.com>
2022-01-19 17:39:58 +0200

    Implemented ip address creation

---
+++ PingPong/PingPong.Common/IP/IPAdressFactory.cs
@@ -0,0 +1,15 @@
+﻿using PingPong.Validation.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Common.IP
+{
+    public class IPAdressFactory
+    {
+        private readonly IAddressValidator
+
+    }
+}
