cb9ebec5c5a3f74deef5949cf768658523ed788e
Electric1447 <itai.levin4@gmail.com>
2022-01-19 17:55:01 +0200

    Forgot to save :(

---
+++ PingPong/PingPong.Common/Dtos/IPAddressWithPort.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace PingPong.Common.Dtos
+{
+    public struct IPAddressWithPort
+    {
+        public IPAddress Address { get; init; }
+        public int Port { get; init; }
+
+        public IPAddressWithPort(IPAddress address, int port)
+        {
+            Address = address;
+            Port = port;
+        }
+
+        public override string ToString() => $"{Address}:{Port}";
+    }
+}
---
+++ PingPong/PingPong.CreationPolicy/IPAdressFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using PingPong.Common.Dtos;
+using PingPong.Validation.Abstractions;
+
+namespace PingPong.CreationPolicy
+{
+    public class IPAdressFactory
+    {
+        private readonly IAddressValidator _addressValidator;
+        private readonly IPortValidator _portValidator;
+
+        public IPAdressFactory(IAddressValidator addressValidator, IPortValidator portValidator)
+        {
+            _addressValidator = addressValidator;
+            _portValidator = portValidator;
+        }
+
+        public IPAddressWithPort CreateAddress(string[] input)
+        {
+            if (input.Length == 2)
+            {
+                if (_addressValidator.TryParseAddress(input[0], out var address) &&
+                    _portValidator.TryParsePort(input[1], out int port))
+                {
+                    return new IPAddressWithPort(new IPAddress(address), port);
+                }
+            }
+
+            throw new Exception();
+        }
+    }
+}
