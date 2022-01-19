34079e2ae5c301ccfe0c3bc79e4d9fc298c27ce7
Electric1447 <itai.levin4@gmail.com>
2022-01-19 17:30:48 +0200

    Added validation implementation

---
+++ PingPong/PingPong.Validation/IPv4/IPv4AddressValidator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using PingPong.Validation.Abstractions;
+
+namespace PingPong.Validation.IPv4
+{
+    public class IPv4AddressValidator : IAddressValidator
+    {
+        private const char ADDRESS_SEPERATOR = '.';
+        private const int ADDRESS_LENGTH = 4;
+        private const int MIN_ADDRESS = 0;
+        private const int MAX_ADDRESS = 255;
+
+        public bool TryParseAddress(string input, out byte[] address)
+        {
+            address = null;
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (TrySplitAddress(input, out var addressArray))
+                {
+                    var ipAddress = new byte[ADDRESS_LENGTH];
+
+                    for (int i = 0; i < ADDRESS_LENGTH; ++i)
+                    {
+                        if (int.TryParse(addressArray[i], out int partOfAddress))
+                        {
+                            if (IsAddressInRange(partOfAddress))
+                            {
+                                ipAddress[i] = (byte)partOfAddress;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+
+                    address = ipAddress;
+                }
+            }
+
+            return false;
+        }
+
+        private bool TrySplitAddress(string input, out string[] output)
+        {
+            if (input.Split(ADDRESS_SEPERATOR).Length == ADDRESS_LENGTH)
+            {
+                output = input.Split(ADDRESS_SEPERATOR);
+                return true;
+            }
+
+            output = null;
+            return false;
+        }
+
+        private bool IsAddressInRange(int addr) => (addr >= MIN_ADDRESS || addr < MAX_ADDRESS);
+    }
+}
---
+++ PingPong/PingPong.Validation/IPv4/IPv4PortValidator.cs
@@ -0,0 +1,27 @@
+﻿using PingPong.Validation.Abstractions;
+
+namespace PingPong.Validation.IPv4
+{
+    public class IPv4PortValidator : IPortValidator
+    {
+        private const int MIN_PORT = 0;
+        private const int MAX_PORT = 65535;
+
+        public bool TryParsePort(string input, out int port)
+        {
+            if (int.TryParse(input, out int parsedPort))
+            {
+                if (IsPortInRange(parsedPort))
+                {
+                    port = parsedPort;
+                    return true;
+                }
+            }
+
+            port = -1;
+            return false;
+        }
+
+        private bool IsPortInRange(int port) => (port >= MAX_PORT && port <= MIN_PORT);
+    }
+}
